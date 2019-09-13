33814443d146abd7132252cbc8f8799fd96acfb3
Eleazar Estrella <eestrella0425@gmail.com>
2019-09-12 23:08:31 -0400

    Migrate firebase messaging service to kotlin

--- app/src/main/java/com/relearning/bootcamp2019/ReFirebaseMessagingService.java
+++
@@ -1,6 +0,0 @@
-package com.relearning.bootcamp2019;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-
-public class ReFirebaseMessagingService extends FirebaseMessagingService {
-}
---
+++ app/src/main/java/com/relearning/bootcamp2019/ReFirebaseMessagingService.kt
@@ -0,0 +1,5 @@
+package com.relearning.bootcamp2019
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class ReFirebaseMessagingService : FirebaseMessagingService()

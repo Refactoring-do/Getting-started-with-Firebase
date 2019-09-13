f569cb076124d2de458d223a2f86236a63eb33b4
Eleazar Estrella <eleazarestrella@Eleazars-MacBook-Pro.local>
2019-09-12 22:50:17 -0400

    first commit

---
+++ app/src/main/java/com/relearning/bootcamp2019/ReFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package com.relearning.bootcamp2019;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class ReFirebaseMessagingService extends FirebaseMessagingService {
+}
